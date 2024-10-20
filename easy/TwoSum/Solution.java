7d2887677f4ba1ef24554281804d28b854543e03
tho <thomasjcabovianco@gmail.com>
2024-10-20 02:03:51 -0300

    Two Sum

---
+++ easy/TwoSum/Solution.java
@@ -0,0 +1,27 @@
+package TwoSum;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int sum = nums[i];
+
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                sum += nums[j];
+                if (sum == target) {
+                    arr[0] = i;
+                    arr[1] = j;
+                    return arr;
+                }
+
+                sum -= nums[j];
+            }
+        }
+
+        return new int[]{};
+    }
+}
